9cb48b11662a131bd552bcd227f0569c3b369e4b
Abidi Ameni <amenia859@gmail.com>
2025-11-08 19:39:04 +0100

    React State Project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
 import React from "react";
-import "./style.css";
-import { Navbar, Nav, Container } from "react-bootstrap";
-import PlayerList from "./PlayersList";
+import { Card, Navbar, Container, Button } from "react-bootstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar bg="primary" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand href="#home" style={{ color: "rgb(20, 3, 68)" }}>
-            FIFA
-          </Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">View Players</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
+class App extends React.Component {
+  state = {
+    fullName: " ",
+    bio: " ",
+    imgSrc: " ",
+    profession: " ",
+    show: false,
+  };
 
-      <h1 className="mb-4 text-center"> FIFA player cards </h1>
-      <PlayerList />
-    </div>
-  );
+  handleClick = () =>
+    this.setState({
+      fullName: "Ameni",
+      bio: "developer",
+      imgSrc:
+        "https://versionweekly.com/wp-content/uploads/2022/03/muslim-girls-images-for-whatsapp-dp.png",
+      profession: "graduated",
+      timer: 0,
+    });
+  handleShow = () => {
+    this.setState({
+      show: !this.state.show,
+    });
+    this.handleClick();
+  };
+  componentDidMount = () => {
+    setInterval(() => {
+      this.setState({ timer: this.state.timer + 1 });
+    }, 1000);
+  };
+  render() {
+    return (
+      <div className="d-grid gap-2">
+        <Navbar className="bg-body-tertiary">
+          <Container>
+            <Navbar.Brand href="#home">
+              <img
+                alt=""
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8m8UuRL2ab_gk9blXR1gyzw0nxSPXXJSQJw&s"
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+              />{" "}
+              PROFILE
+            </Navbar.Brand>
+          </Container>
+        </Navbar>
+        <Button variant="secondary" size="lg" onClick={this.handleShow}>
+          Show Profile {this.state.show ? "Hide Profile" : "Show Profile"}
+        </Button>
+
+        {this.state.show && (
+          <>
+            <Card className="text-center">
+              <Card.Body>
+                <Card.Img
+                  variant="top"
+                  src={this.state.imgSrc}
+                  style={{ width: "250px", height: "250px" }}
+                />
+                <Card.Subtitle
+                  className="mb-2 text-muted"
+                  style={{ marginTop: "10px" }}
+                >
+                  {" "}
+                  Name : {this.state.fullName}
+                </Card.Subtitle>
+                <Card.Text>Bio : {this.state.bio} </Card.Text>
+                <Card.Text>Profession : {this.state.profession} </Card.Text>
+                <Card.Text>Time Interval : {this.state.timer} </Card.Text>
+              </Card.Body>
+            </Card>
+          </>
+        )}
+      </div>
+    );
+  }
 }
-
 export default App;
